14a5bc26471fda09cfa711305f50fe4d5a0520f4
issa <issa.elkhaoua2001@gmail.com>
2023-07-14 13:09:02 +0100

    retour

--- src/app/components/graphe/graphe.component.ts
+++ src/app/components/graphe/graphe.component.ts
@@ -7,29 +7,6 @@ import { Chart} from 'angular-highcharts';
   templateUrl: './graphe.component.html',
   styleUrls: ['./graphe.component.css']
 })
-export class GrapheComponent implements OnInit {
-  debug: boolean = false;
-  cursorGrabbed: boolean = false;
-  gameOver: boolean = false;
-
-  ngOnInit() {}
-
-  handleToggleDebug() {
-    this.debug = !this.debug;
-  }
-
-  handleCursorGrabbed() {
-    this.cursorGrabbed = true;
-    setTimeout(() => {
-      this.cursorGrabbed = false;
-    }, 2000);
-  }
-
-  handleButtonClicked() {
-    this.gameOver = true;
-    setTimeout(() => {
-      this.gameOver = false;
-    }, 4000);
-  }
-  
+export class GrapheComponent {
+ 
 }
